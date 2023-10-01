56cddb24633d689736573b4cbf4da6da95bd9eed
Mateusz Jamróz <jamr.mat@gmail.com>
2023-10-01 08:55:41 +0200

    implement multi language processing

--- service/executors/LanguageExecutor.py
+++ service/executors/LanguageExecutor.py
@@ -1,8 +1,15 @@
+from googletrans import Translator
+
 from context.Context import Context
 from service.executors.Executor import Executor
 
 
 class LanguageExecutor(Executor):
 
+    def __init__(self):
+        self.translator = Translator()
+
     def execute(self, context: Context):
-        pass
+        if context.language != 'PL':
+            translation = translator.translate(context.request, src='pl', dst='de')
+            context.request = translation
